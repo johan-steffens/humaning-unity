79f6bd19f8e296a71dd016115ce5672a2ab1e3f8
= <=>
2019-04-25 06:59:23 +0200

    refactor: modified encounter screen to be bit more simple

--- Assets/Scripts/Game/EncounterSceneController.cs
+++ Assets/Scripts/Game/EncounterSceneController.cs
@@ -6,23 +6,17 @@ using UnityEngine.UI;
 
 public class EncounterSceneController : MonoBehaviour
 {
+    public Image sliderFill;
+    public Slider slider;
 
-    public Text wText;
-    public Text eText;
-    public Text aText;
-    public Text sText;
-    public Text dText;
-    public Text zText;
-    public Text xText;
-
-    public Color hint = Color.blue;
-    public Color activated = Color.green;
-    public Color error = Color.red;
-    public Color deactivated = Color.black;
+    public Color filledColor = Color.green;
+    public Color emptyColor = Color.red;
 
     public Text weightValue;
     public Text fatPercentageValue;
     public Text genderValue;
+
+    public float debugValue;
 
     private static EncounterSceneController instance;
 
@@ -54,57 +48,7 @@ public class EncounterSceneController : MonoBehaviour
 
     void Update()
     {
-        Color wColor = hint;
-        Color eColor = deactivated;
-        Color aColor = deactivated;
-        Color sColor = deactivated;
-        Color dColor = deactivated;
-        Color zColor = deactivated;
-        Color xColor = deactivated;
-
-        // W Key
-        if (Input.GetKey(KeyCode.W))
-        {
-            wColor = activated;
-        }
-        // E Key
-        else if (Input.GetKey(KeyCode.E))
-        {
-            eColor = activated;
-        }
-        // A Key
-        else if (Input.GetKey(KeyCode.A))
-        {
-            aColor = activated;
-        }
-        // S Key
-        else if (Input.GetKey(KeyCode.S))
-        {
-            sColor = activated;
-        }
-        // D Key
-        else if (Input.GetKey(KeyCode.D))
-        {
-            dColor = activated;
-        }
-        // Z Key
-        else if (Input.GetKey(KeyCode.Z))
-        {
-            zColor = activated;
-        }
-        // X Key
-        else if (Input.GetKey(KeyCode.X))
-        {
-            xColor = activated;
-        }
-
-        // Set colors
-        wText.color = wColor;
-        eText.color = eColor;
-        aText.color = aColor;
-        sText.color = sColor;
-        dText.color = dColor;
-        zText.color = zColor;
-        xText.color = xColor;
+        slider.value = debugValue;
+        sliderFill.color =  Color.Lerp(emptyColor, filledColor, (float) debugValue / 1f);
     }
 }
